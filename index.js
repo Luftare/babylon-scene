f13658d6593c7d992820d615b933d45ecc67e0b3
Ilmari <ilmari.koskinen1@gmail.com>
2018-12-18 13:11:15 +0200

    Bounce from trees

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import {
   StandardMaterial,
   SceneLoader,
   HemisphericLight,
+  MeshBuilder,
 } from 'babylonjs';
 
 import heightmap from './heightmap.png';
@@ -24,7 +25,13 @@ const HEIGHT = 1023;
 const canvas = document.querySelector('canvas');
 const engine = new Engine(canvas);
 const scene = new Scene(engine);
+scene.collisionsEnabled = true;
+
+const cameraCollider = new Mesh.CreateSphere('cameraCollider', 32, 2, scene);
+cameraCollider.checkCollisions = true;
+
 const camera = new FreeCamera('camera', new Vector3(0, 30, 0), scene);
+
 const hemisphericLight = new HemisphericLight(
   'hemiLight',
   new Vector3(0, 1, 0),
@@ -37,6 +44,8 @@ const directionalLight = new DirectionalLight(
   scene
 );
 directionalLight.intensity = 0.3;
+
+const pines = [];
 
 const groundMaterial = new StandardMaterial('ground', scene);
 const ground = Mesh.CreateGroundFromHeightMap(
@@ -52,12 +61,21 @@ const ground = Mesh.CreateGroundFromHeightMap(
   () => {
     SceneLoader.ImportMesh('', '/', pineMesh.substr(1), scene, newMeshes => {
       ground.updateCoordinateHeights();
-      for (let i = 0; i < 600; i++) {
+      for (let i = 0; i < 100; i++) {
         const x = (Math.random() - 0.5) * WIDTH;
         const z = (Math.random() - 0.5) * HEIGHT;
         const y = ground.getHeightAtCoordinates(x, z);
         const position = new Vector3(x, y, z);
-        const pine = new Mesh.CreateBox(`pine-${i}`, 1, scene);
+        const pine = new MeshBuilder.CreateCylinder(
+          `pine-${i}`,
+          { height: 16, diameter: 5 },
+          scene
+        );
+
+        pine.checkCollisions = true;
+
+        pines.push(pine);
+
         newMeshes.forEach((mesh, i) => {
           const clonedMesh = mesh.clone();
           clonedMesh.parent = pine;
@@ -95,7 +113,7 @@ window.addEventListener('keyup', ({ key }) => {
   keysDown[key] = false;
 });
 
-const gravity = 0.1;
+const gravity = 0.03;
 let cameraVelocityY = 0;
 
 function rotateVectorY(vector, angle) {
@@ -105,38 +123,79 @@ function rotateVectorY(vector, angle) {
   );
 }
 
-function init() {
-  engine.runRenderLoop(() => {
-    const { a: left, d: right, w: up, s: down, ' ': space } = keysDown;
+function handleCollisions() {
+  cameraCollider.position = camera.position.clone();
+  pines.forEach(pine => {
+    const roughlyIntersects = cameraCollider.intersectsMesh(pine, false);
+    if (roughlyIntersects) {
+      const intersects = cameraCollider.intersectsMesh(pine, true);
+      if (intersects) {
+        const fromPineToCamera = camera.position.subtract(pine.position);
+        const projectedDistance = fromPineToCamera
+          .clone()
+          .set(fromPineToCamera.x, 0, fromPineToCamera.z)
+          .length();
 
-    const inputVector = new Vector3(0, 0, 0);
-    if (right) inputVector.addInPlace(new Vector3(1, 0, 0));
-    if (left) inputVector.addInPlace(new Vector3(-1, 0, 0));
-    if (up) inputVector.addInPlace(new Vector3(0, 0, 1));
-    if (down) inputVector.addInPlace(new Vector3(0, 0, -1));
-    if (space) cameraVelocityY = 2;
-
-    const walkVelocity = rotateVectorY(
-      inputVector,
-      camera.rotation.y
-    ).normalize();
-
-    cameraVelocityY -= gravity;
-
-    camera.position.addInPlace(walkVelocity);
-    camera.position.y += cameraVelocityY;
-
-    ground.updateCoordinateHeights();
-
-    const height = ground.getHeightAtCoordinates(
-      camera.position.x,
-      camera.position.z
-    );
-
-    if (height) {
-      camera.position.y = Math.max(camera.position.y, 3 + height);
+        const pineRadius = 3;
+        const playerRadius = 2;
+        const intersectionDepth = Math.max(
+          0,
+          pineRadius + playerRadius - projectedDistance
+        );
+        camera.position.addInPlace(
+          fromPineToCamera.normalize().scaleInPlace(intersectionDepth * 0.2)
+        );
+      }
     }
+  });
+}
 
+function applyPhysics() {
+  cameraVelocityY -= gravity;
+  camera.position.y += cameraVelocityY;
+
+  const groundY = ground.getHeightAtCoordinates(
+    camera.position.x,
+    camera.position.z
+  );
+
+  const cameraOffsetY = 2;
+  const isTouchingGround = camera.position.y - cameraOffsetY <= groundY;
+
+  if (isTouchingGround) {
+    cameraVelocityY = 0;
+    camera.position.y = groundY + cameraOffsetY;
+    camera.grounded = true;
+  }
+}
+
+function handleInput() {
+  const { a: left, d: right, w: up, s: down, ' ': space } = keysDown;
+
+  const inputVector = new Vector3(0, 0, 0);
+  if (right) inputVector.addInPlace(new Vector3(1, 0, 0));
+  if (left) inputVector.addInPlace(new Vector3(-1, 0, 0));
+  if (up) inputVector.addInPlace(new Vector3(0, 0, 1));
+  if (down) inputVector.addInPlace(new Vector3(0, 0, -1));
+  if (space && camera.grounded) {
+    cameraVelocityY = 0.5;
+    camera.grounded = false;
+  }
+
+  const walkVelocity = rotateVectorY(inputVector, camera.rotation.y)
+    .normalize()
+    .scaleInPlace(0.2);
+  camera.position.addInPlace(walkVelocity);
+}
+
+function init() {
+  scene.registerBeforeRender(() => {
+    handleInput();
+    applyPhysics();
+    handleCollisions();
+  });
+
+  engine.runRenderLoop(() => {
     scene.render();
   });
 }
